f62c06af9c99286d54942c6f921bb04eec6eea89
dragsec <drag93@live.it>
2023-09-12 10:12:52 +0200

    test commit

--- src/main/java/io/dragsec/gym/gymmanager/Main.java
+++ src/main/java/io/dragsec/gym/gymmanager/Main.java
@@ -1,7 +1,14 @@
 package io.dragsec.gym.gymmanager;
 
+import io.dragsec.gym.gymmanager.model.User;
+
 public class Main {
     public static void main(String[] args) {
-        //TODO
+        System.out.println("Hello World");
+        User user = new User("Alex", "Rossi", 20);
+        System.out.println(user);
+        User user2 = new User("Nico", "Rogai", 31);
+        System.out.println(user2);
+        String persona1Nome = user.firstname;
     }
 }
