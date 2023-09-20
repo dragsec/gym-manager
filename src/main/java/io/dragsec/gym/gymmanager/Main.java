3f38294570f7c54843a4e60cd1ffb3b04929f4a9
dragsec <drag93@live.it>
2023-09-20 19:45:16 +0200

    user getter and setter

--- src/main/java/io/dragsec/gym/gymmanager/Main.java
+++ src/main/java/io/dragsec/gym/gymmanager/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(user);
         User user2 = new User("Nico", "Rogai", 31);
         System.out.println(user2);
-        String persona1Nome = user.firstname;
+        String bodyEmail = "Gentile signor. " + user.getFirstname();
+        System.out.println(bodyEmail);
     }
 }
--- src/main/java/io/dragsec/gym/gymmanager/model/User.java
+++ src/main/java/io/dragsec/gym/gymmanager/model/User.java
@@ -1,13 +1,29 @@
 package io.dragsec.gym.gymmanager.model;
 
 public class User {
-    public String firstname;
-    public String lastname;
-    public int age;
+    private String firstname;
+    private String lastname;
+    private int age;
 
     public User(String firstname, String lastname, int age) {
         this.firstname = firstname;
         this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
